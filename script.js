1cdd7918162ed1cd1650e69db714b8b2cb039c23
Akanjii <65922110+Akanjii@users.noreply.github.com>
2025-03-07 18:54:28 +0100

    created function to generate computer choice

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function getHumanChoice() {
-  let choice = prompt('Pick a weapon');
-  return choice;
+  let humanChoice = prompt('Pick a weapon');
+  return humanChoice;
 }
 
 console.log(getHumanChoice())
+
+
+function getComputerChoice() {
+  let randomNumber = Math.random();
+  let computerChoice = '';
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3 ) {
+    computerChoice = 'rock';
+  } else if ( randomNumber >= 1 / 3 && randomNumber < 2 / 3 ) {
+    computerChoice = 'paper';
+  } else if ( randomNumber >= 2 / 3 && randomNumber < 1 ) {
+    computerChoice = 'scissors';
+  }
+
+  return computerChoice;
+
+};
+console.log(getComputerChoice())
